027c940390bc77d30f0ef1b33dcc5ebac66422af
Derevianko Liubov <dereviankoliubov@gmail.com>
2015-12-08 22:22:52 +0200

    remove devise sign_up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
   get 'welcome/index'
   root 'welcome#index'
+
+  #for any routing errors this will redirec to home page
+  get '*path' => redirect('/')
 end
